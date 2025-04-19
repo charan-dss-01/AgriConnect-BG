4083a8f2156ccc3483199230b1c62751d5b22236
charan-dss-01 <agriconnect8899@gmail.com>
2025-04-19 10:07:49 +0530

    Update authUser.js

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken"
 
 export const isAuthenticated = async (req, res, next) => {
     try {
-        const token = req.cookies.jwt;
+        const token = req.cookies.jwt|| req.headers.authorization?.split(' ')[1];
         console.log("Middleware token:", token);
         if (!token) {
             return res.status(401).json({ error: "No token provided, authorization denied" });
